191049a8d6dadccdbde7cb956aaf14df0eb0ddfe
dalilataldo <dalila.taldo@gmail.com>
2024-05-17 23:25:56 +0200

    add: snack 7

---
+++ snack-7/main.js
@@ -0,0 +1,25 @@
+// //variabili di partenza con maxnumber dato
+// let start = 1;
+// let maxnumber = 1000;
+// let maxvalue = maxnumber / 2;
+
+// //comando dell'azione: stampa i numeri pari fino al maxnumber
+// for (let i = start; i <= maxvalue; i++) {
+
+//     let evennumbers = i * 2;
+//     console.log(evennumbers)
+
+// }
+
+//variabili di partenza con maxnumber da chiedere all'utente
+let start = 1;
+let maxnumber = Number(prompt('Inserisci un numero massimo'));
+let maxvalue = maxnumber / 2;
+
+//comando dell'azione: stampa i numeri pari fino al maxnumber
+for (let i = start; i <= maxvalue; i++) {
+
+    let evennumbers = i * 2;
+    console.log(evennumbers)
+
+}
---
+++ snack-7/readme.md
@@ -0,0 +1,8 @@
+Scrivere un programma che stampi la tabellina del 2, fino al numero 1000.
+Modificate poi il programma in modo che venga chiesto all'utente il numero massimo consentito, e stampare tutta la tabellina del 2 fino al numero inserito.
+
+1 caso: num * 2 <= 1000
+2 caso: chiedi numero max lenght
+num * 2 <= max lenght
+
+stamp
